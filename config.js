3ca3c8882ae1078ed01985a04f5855f649da900c
slateding <mikezhangding@gmail.com>
2018-03-23 13:27:30 +0800

    init2

---
+++ config.js
@@ -0,0 +1,21 @@
+
+var config = {
+    db: {
+        options: {
+            // db: {native_parser: true}, // 过时不适用
+            // server: {poolSize: 5},
+            socketTimeoutMS: 0,
+            keepAlive: true,
+            reconnectTries: 30
+//  replset: { rs_name: 'myReplicaSetName' },
+//  user: 'admin',
+//  pass: '123456'
+        },
+        uri: process.env.MONGO_URL || 'mongodb://127.0.0.1/hobbydb'
+    },
+    porthttp: process.env.PORT || 3311,
+    multicore: false,
+    https: true,
+    debug: false
+}
+module.exports = config;
